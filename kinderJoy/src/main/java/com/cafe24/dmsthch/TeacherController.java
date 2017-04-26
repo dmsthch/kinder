a3b828e5985255129ffe815044b2073fff7372ee
201-15 <201-15@201-15-PC>
2017-04-26 16:05:22 +0900

    맵퍼파일 추가 컨트롤러 인서트 경로설정 Dao에 insert추가 DB 연동 완료

--- kinderJoy/src/main/java/com/cafe24/dmsthch/TeacherController.java
+++ kinderJoy/src/main/java/com/cafe24/dmsthch/TeacherController.java
@@ -1,13 +1,18 @@
 package com.cafe24.dmsthch;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cafe24.dmsthch.Teacher.Teacher;
+import com.cafe24.dmsthch.Teacher.TeacherDao;
 
 @Controller
 public class TeacherController {
+	
+	@Autowired
+	private TeacherDao TDao;
 	
 	@RequestMapping(value="/Add", method=RequestMethod.GET)
 	public String Add() {
@@ -17,8 +22,8 @@ public class TeacherController {
 	
 	@RequestMapping(value="/insert", method=RequestMethod.POST)
 	public String insert(Teacher teacher) {//매개변수는 전역변수이다
-		System.out.println(teacher +"<--전역변수");
-		
+		TDao.insertTeacher(teacher);
+		System.out.println(teacher+" <--입력확인");
 		return "home";
 	}
 }
