c48134b9afb90d0bab8bb20213627720ea0e75e2
jkc <jkc@jkc-PC>
2017-05-06 09:02:11 +0900

    로그인 후 뒤로가기 시 다시 로그인 화면 나타나는 현상 해결 (브라우저 캐싱방지) 양식 다시 제출 확인 페이지 나타나는 현상 해결 (redirect로 해결)

--- kinderJoy/src/main/java/com/cafe24/dmsthch/TeacherController.java
+++ kinderJoy/src/main/java/com/cafe24/dmsthch/TeacherController.java
@@ -2,7 +2,6 @@ package com.cafe24.dmsthch;
 
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
 import com.cafe24.dmsthch.Teacher.Teacher;
 import com.cafe24.dmsthch.Teacher.TeacherDao;
 
@@ -29,7 +27,7 @@ public class TeacherController extends HandlerInterceptorAdapter {
 	private TeacherDao TDao;
 	
 
-	@Override
+/*	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 	     Object session = request.getSession().getAttribute("teacherId");
 	     if (session == null) {
@@ -37,7 +35,7 @@ public class TeacherController extends HandlerInterceptorAdapter {
 	    	 //세션의 값이 null일 경우 홈페이지로 이동한다
 	      }
 		return true;
-    }
+    }*/
 	
 	
 	@RequestMapping(value="/Add", method=RequestMethod.GET)
@@ -79,11 +77,18 @@ public class TeacherController extends HandlerInterceptorAdapter {
 		if(session.getAttribute("teacherId") == null){
 			return "Teacher/TeacherLogin";
 		}else{
-			return "home";
+			return "redirect:/home";
 		}
 	}
 	
 	
+	//로그인 시 캐시삭제를 하여서 뒤로가기해도 로그인폼이 안뜨게 해결 했었지만
+	//양식 다시 제출 확인 페이지가 떠서 문제였었는데
+	//이유가 forward하여서 이다.
+	//forward는 웹브라우저의 url은 그대로 놔둔 상태에서 서버단에서 페이지의 이동을 해버리기 때문에 
+	//웹브라우저에서 reload하면 웹브라우저에 남아있는 url을 reload하게되어 문제가 발생한 것이었다
+	//redirect를 하면 해당 url을 클라이언트 브라우저에 내려줘서 브라우저가 해당 url로 이동하게끔 시켜주기 때문에 브라우저의 url도 바뀌게 된다
+	//그래서 reload를 해도 바뀐 url로 reload를 하게 된다
 	@RequestMapping(value="/Login" , method = RequestMethod.POST)
 	public String Login(HttpServletRequest request, Model model,Teacher teacher,HttpSession session) {
 		System.out.println("Teacher 컨트롤러 로그인 메서드 확인");
@@ -110,7 +115,7 @@ public class TeacherController extends HandlerInterceptorAdapter {
 		model.addAttribute("teacherLevel" ,saveSession.getTeacher_level());
 		System.out.println(saveSession.getTeacher_level() + " <--세션에 저장될 레벨값");
 		
-		System.out.println(session.getId() +"<--세션의 아이디값");
+		System.out.println(session.getId() +"<--생성된 세션의 아이디");
 		System.out.println( session.getMaxInactiveInterval()+"<--세션의 유지 시간 / 단위 : 초");
 		
 		session.setMaxInactiveInterval(600);
@@ -122,7 +127,7 @@ public class TeacherController extends HandlerInterceptorAdapter {
 		//HttpSession getsession = se.getSession();
 		//System.out.println("생성된 세션값 "+getsession.getId());
 		}
-		return "home";
+		return "redirect:/home";
 	}
 	
 	@RequestMapping(value="/index", method=RequestMethod.GET)
