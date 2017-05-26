7a6283bc458226384c8d79b41fd9234186902da2
Administrator <Administrator@df-gvj>
2017-05-26 17:17:45 +0900

    123

---
+++ kinderJoy/src/main/java/com/cafe24/dmsthch/Equipment/EquipmentValue.java
@@ -0,0 +1,46 @@
+package com.cafe24.dmsthch.Equipment;
+
+public class EquipmentValue {
+	private String valueNo;
+	private String equipmentNo;
+	private String equipmentCost;
+	private String equipmentAmount;
+	private String equipmentCustomer;
+	private String equipmentDay;
+	public String getValueNo() {
+		return valueNo;
+	}
+	public void setValueNo(String valueNo) {
+		this.valueNo = valueNo;
+	}
+	public String getEquipmentNo() {
+		return equipmentNo;
+	}
+	public void setEquipmentNo(String equipmentNo) {
+		this.equipmentNo = equipmentNo;
+	}
+	public String getEquipmentCost() {
+		return equipmentCost;
+	}
+	public void setEquipmentCost(String equipmentCost) {
+		this.equipmentCost = equipmentCost;
+	}
+	public String getEquipmentAmount() {
+		return equipmentAmount;
+	}
+	public void setEquipmentAmount(String equipmentAmount) {
+		this.equipmentAmount = equipmentAmount;
+	}
+	public String getEquipmentCustomer() {
+		return equipmentCustomer;
+	}
+	public void setEquipmentCustomer(String equipmentCustomer) {
+		this.equipmentCustomer = equipmentCustomer;
+	}
+	public String getEquipmentDay() {
+		return equipmentDay;
+	}
+	public void setEquipmentDay(String equipmentDay) {
+		this.equipmentDay = equipmentDay;
+	}
+}
